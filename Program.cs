fba5ca884a6047a37957cef0808db212c27de78a
Abdullah Oyedele <abdullahoyedele2019@gmail.com>
2023-01-21 23:32:19 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,32 @@
-using System; //Prime Factorization
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 class Solution {
 
     static void Main(String[] args) {
-        int t = Convert.ToInt32(Console.ReadLine());
+        int t = int.Parse(Console.ReadLine());
         for(int a0 = 0; a0 < t; a0++){
             int n = Convert.ToInt32(Console.ReadLine());
-            
+            int num = 1;
+            bool isDivisbleByAll = false;
+            bool isFound = false;
+            while (isFound == false) {
+                for (int i = 1; i <= n; i++) {
+                    if (num % i == 0) {
+                        isDivisbleByAll = true;
+                    }
+                    else {
+                        isDivisbleByAll = false;
+                        break;
+                    }
+                }
+                if (isDivisbleByAll) {
+                    isFound = true;
+					Console.WriteLine(num);
+                }
+                num++;
+            }
         }
     }
 }
